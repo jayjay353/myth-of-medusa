0d15609fde877ef4922e4965e429fed7531c497f
Achira <jamieleelambert386@gmail.com>
2026-05-09 16:04:44 +0200

    Erster Build Athen 3D

---
+++ scripts/god_controller.gd
@@ -0,0 +1,20 @@
+extends CharacterBody3D
+
+@export var speed := 5.0
+@export var gravity := 9.8
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
+	
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+	
+	move_and_slide()
